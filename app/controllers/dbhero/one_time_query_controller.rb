5a56765daf953b2e68d4aafc310db47a2bef82b2
John Mark Untalan <jm@aelogica.com>
2018-01-15 09:43:07 +0800

    [#154232729] Add one_time_query feature for Integrations Reports

---
+++ app/controllers/dbhero/one_time_query_controller.rb
@@ -0,0 +1,29 @@
+require_dependency "dbhero/application_controller"
+
+module Dbhero
+  class OneTimeQueryController < ApplicationController
+    before_action :check_auth
+    respond_to :html, :csv
+
+    def index
+      if params.dig(:dataclip, :raw_query).present?
+        @dataclip = Dataclip.new(dataclip_query_params.merge(one_time_query: true))
+      else
+        @dataclip = Dataclip.new
+      end
+
+      respond_to do |format|
+        format.html
+        format.csv do
+          send_data @dataclip.csv_string, type: Mime::CSV, disposition: "attachment; filename=one_time_query-#{DateTime.now.strftime("%Y%m%d%H%M")}.csv"
+        end
+      end
+    end
+
+    private
+
+    def dataclip_query_params
+      params.require(:dataclip).permit(:raw_query, :one_time_query)
+    end
+  end
+end
